3a4a99a5bfa27736c2d3335054eba2adb82b7ec1
adhi0987 <adhithyashivakumar2511@gmail.com>
2025-05-27 19:40:34 +0530

    initial commit

---
+++ portfolioWebsite/src/components/Greeting.tsx
@@ -0,0 +1,11 @@
+// src/components/Greeting.tsx
+
+type GreetingProps = {
+  name: string;
+};
+
+const Greeting: React.FC<GreetingProps> = ({ name }) => {
+  return <h2>Hello, {name}!</h2>;
+};
+
+export default Greeting;
