c78d65be64b057ae6db557b8de9cb30a71e39095
adhi0987 <adhithyashivakumar2511@gmail.com>
2025-06-11 21:38:33 +0530

    added navbar links

--- portfolioWebsite/src/App.tsx
+++ portfolioWebsite/src/App.tsx
@@ -1,42 +1,51 @@
-
-import { useEffect, useState } from 'react';
-import '../styles/App.css';
-import Navbar from './components/Navbar';
-import MainPage from './components/MainPage';
-import AcademicSection from './components/AcademicSection';
-import ProjectSection from './components/ProjectSection';
-import Internship from './components/Internship';
-import Footer from './components/Footer';
+import { useEffect, useState } from "react";
+import "../styles/App.css";
+import AcademicSection from "./components/AcademicSection";
+import Footer from "./components/Footer";
+import Internship from "./components/Internship";
+import MainPage from "./components/MainPage";
+import Navbar from "./components/Navbar";
+import ProjectSection from "./components/ProjectSection";
 
 function App() {
-  const [toggleTheme , setToggleTheme] = useState(false);
-  useEffect(()=>{
-    if(toggleTheme)
-    {
-      document.body.setAttribute('data-theme',"dark");
-    }else
-    {
-      document.body.setAttribute('data-theme',"light");
+  const [toggleTheme, setToggleTheme] = useState(false);
+  useEffect(() => {
+    if (toggleTheme) {
+      document.body.setAttribute("data-theme", "dark");
+    } else {
+      document.body.setAttribute("data-theme", "light");
     }
   });
-  const changeTheme = ()=>{
+  const changeTheme = () => {
     setToggleTheme(!toggleTheme);
-  }
+  };
   return (
     <>
-      <div className='navbar'><Navbar toggleValue ={toggleTheme} themeFunction={changeTheme}/></div>
-      <div><MainPage toggler={toggleTheme}/></div>
+      <div className="navbar">
+        <Navbar toggleValue={toggleTheme} themeFunction={changeTheme} />
+      </div>
+      <div id="home">
+        <MainPage toggler={toggleTheme} />
+      </div>
       <hr />
-      <div><AcademicSection/></div>
+      <div id="acads"> 
+        <AcademicSection />
+      </div>
       <hr />
-      <div><ProjectSection/></div>
+      <div id="projects">
+        <ProjectSection />
+      </div>
       <hr />
-      <div><Internship/></div>
+      <div id="internship">
+        <Internship />
+      </div>
       <hr />
-      <div><Footer/></div>
+      <div>
+        <Footer />
+      </div>
       {/* <Greeting name='adhithya'/> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- portfolioWebsite/src/components/Navbar.tsx
+++ portfolioWebsite/src/components/Navbar.tsx
@@ -1,31 +1,28 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMoon, faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
-import "../../styles/Navbar.css";
-import { useEffect, useState } from "react";
+import { faBars, faMoon, faTimes } from "@fortawesome/free-solid-svg-icons";
 import { faSun } from "@fortawesome/free-solid-svg-icons/faSun";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useEffect, useState } from "react";
+import "../../styles/Navbar.css";
 
-interface Props 
-{
-   themeFunction :()=> void;
-   toggleValue : boolean;
+interface Props {
+  themeFunction: () => void;
+  toggleValue: boolean;
+  
 }
 
-
-function Navbar(props:Props) {
+function Navbar(props: Props) {
   const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
-  
+
   const toggleMenu = () => {
     setMobileMenuOpen(!isMobileMenuOpen);
   };
-  useEffect (()=>{
-    if(props.toggleValue)
-    {
-      document.body.setAttribute('data-theme',"dark");
-    }else
-    {
-      document.body.setAttribute('data-theme',"light");
+  useEffect(() => {
+    if (props.toggleValue) {
+      document.body.setAttribute("data-theme", "dark");
+    } else {
+      document.body.setAttribute("data-theme", "light");
     }
-  })
+  });
 
   return (
     <div className="navbar-container">
@@ -34,16 +31,27 @@ function Navbar(props:Props) {
       <div className={`nav-right ${isMobileMenuOpen ? "open" : ""}`}>
         <div className="list">
           <ul>
-            <li>Home</li>
-            <li>Academics</li>
-            <li>Projects</li>
+            <li>
+              <a href="#home">Home</a>{" "}
+            </li>
+            <li>
+              <a href="#acads">Academics</a>
+            </li>
+            <li>
+              <a href="#projects">Projects</a>
+            </li>
           </ul>
         </div>
-        <div className="theme-btn" >
-          {props.toggleValue ?(<button onClick={props.themeFunction}>
-            <FontAwesomeIcon icon={faMoon} style={{ color: "#ffffff" }} />
-          </button>):(<button onClick={props.themeFunction}><FontAwesomeIcon icon={faSun} style={{color: "#000000",}} /></button>)}
-          
+        <div className="theme-btn">
+          {props.toggleValue ? (
+            <button onClick={props.themeFunction}>
+              <FontAwesomeIcon icon={faMoon} style={{ color: "#ffffff" }} />
+            </button>
+          ) : (
+            <button onClick={props.themeFunction}>
+              <FontAwesomeIcon icon={faSun} style={{ color: "#000000" }} />
+            </button>
+          )}
         </div>
       </div>
 
